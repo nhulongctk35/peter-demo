5b5040c267ee9f1dd67f2b7fb16fa8d4d11a1aac
Long Tran <nhulongctk10@gmail.com>
2019-04-15 10:01:20 +0700

    [feature/init] Reformat code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, {Component}             from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-import HomePage from './containers/HomePage'
+import HomePage    from './containers/HomePage'
 import ProfilePage from './containers/Profile'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" component={HomePage}/>
-            <Route path="/:id" component={ProfilePage}/>
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" exact component={HomePage}/>
+                        <Route path="/:id" component={ProfilePage}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
