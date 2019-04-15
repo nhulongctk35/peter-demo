f40685515da3d620e551e5233fdbcea707e81ba6
Long Tran <nhulongctk10@gmail.com>
2019-04-15 10:00:08 +0700

    [feature/init] Init basic route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import HomePage from './containers/HomePage'
+import ProfilePage from './containers/Profile'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>lorem</p>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={HomePage}/>
+            <Route path="/:id" component={ProfilePage}/>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
